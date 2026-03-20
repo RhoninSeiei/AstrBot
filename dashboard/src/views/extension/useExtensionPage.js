5ebe1fd14b128b2f481e665fc91d5222440439bb
RhoninSeiei <33801807+RhoninSeiei@users.noreply.github.com>
2026-03-20 21:53:31 +0000

    feat(extension): add list view and page size options to plugin market

--- dashboard/src/views/extension/useExtensionPage.js
+++ dashboard/src/views/extension/useExtensionPage.js
@@ -182,6 +182,24 @@ export const useExtensionPage = () => {
   const marketSearch = ref("");
   const debouncedMarketSearch = ref("");
   const refreshingMarket = ref(false);
+  const getInitialMarketListViewMode = () => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      return localStorage.getItem("pluginMarketListViewMode") === "true";
+    }
+    return false;
+  };
+  const marketIsListView = ref(getInitialMarketListViewMode());
+  const getInitialMarketItemsPerPage = () => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const rawValue = Number(localStorage.getItem("pluginMarketItemsPerPage"));
+      if ([9, 25, 50, 100].includes(rawValue)) {
+        return rawValue;
+      }
+    }
+    return 9;
+  };
+  const marketItemsPerPageOptions = [9, 25, 50, 100];
+  const marketItemsPerPage = ref(getInitialMarketItemsPerPage());
   const sortBy = ref("default"); // default, stars, author, updated
   const sortOrder = ref("desc"); // desc (降序) or asc (升序)
   const randomPluginNames = ref([]);
@@ -280,6 +298,39 @@ export const useExtensionPage = () => {
 
     return items;
   });
+
+  const marketPluginHeaders = computed(() => [
+    {
+      title: tm("table.headers.name"),
+      key: "name",
+      sortable: false,
+      width: "26%",
+    },
+    {
+      title: tm("table.headers.description"),
+      key: "desc",
+      sortable: false,
+      width: "40%",
+    },
+    {
+      title: tm("table.headers.version"),
+      key: "version",
+      sortable: false,
+      width: "12%",
+    },
+    {
+      title: tm("table.headers.author"),
+      key: "author",
+      sortable: false,
+      width: "10%",
+    },
+    {
+      title: tm("table.headers.actions"),
+      key: "actions",
+      sortable: false,
+      width: "12%",
+    },
+  ]);
 
   // 过滤要显示的插件
   const filteredExtensions = computed(() => {
@@ -430,15 +481,15 @@ export const useExtensionPage = () => {
   };
 
   // 分页计算属性
-  const displayItemsPerPage = 9; // 固定每页显示9个卡片（3行）
+  const displayItemsPerPage = computed(() => marketItemsPerPage.value);
 
   const totalPages = computed(() => {
-    return Math.ceil(sortedPlugins.value.length / displayItemsPerPage);
+    return Math.ceil(sortedPlugins.value.length / displayItemsPerPage.value);
   });
 
   const paginatedPlugins = computed(() => {
-    const start = (currentPage.value - 1) * displayItemsPerPage;
-    const end = start + displayItemsPerPage;
+    const start = (currentPage.value - 1) * displayItemsPerPage.value;
+    const end = start + displayItemsPerPage.value;
     return sortedPlugins.value.slice(start, end);
   });
 
@@ -1721,6 +1772,19 @@ export const useExtensionPage = () => {
     }
   });
 
+  watch(marketIsListView, (newVal) => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.setItem("pluginMarketListViewMode", String(newVal));
+    }
+  });
+
+  watch(marketItemsPerPage, (newVal) => {
+    currentPage.value = 1;
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.setItem("pluginMarketItemsPerPage", String(newVal));
+    }
+  });
+
   watch(
     marketCategoryItems,
     (newItems) => {
@@ -1792,6 +1856,7 @@ export const useExtensionPage = () => {
     upload_file,
     uploadTab,
     showPluginFullName,
+    marketIsListView,
     marketSearch,
     debouncedMarketSearch,
     refreshingMarket,
@@ -1801,6 +1866,7 @@ export const useExtensionPage = () => {
     normalizeStr,
     toPinyinText,
     toInitials,
+    marketPluginHeaders,
     filteredExtensions,
     filteredPlugins,
     filteredMarketPlugins,
@@ -1809,6 +1875,8 @@ export const useExtensionPage = () => {
     randomPlugins,
     shufflePlugins,
     refreshRandomPlugins,
+    marketItemsPerPage,
+    marketItemsPerPageOptions,
     displayItemsPerPage,
     totalPages,
     paginatedPlugins,
